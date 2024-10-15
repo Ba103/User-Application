b4d64137716e0528f604967a2925d6705503ff4a
balaji <balajir9966@gmail.com>
2024-10-15 14:45:25 +0530

    added delete method

--- src/main/java/in/balaji/rest/UserRestController.java
+++ src/main/java/in/balaji/rest/UserRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,6 +37,13 @@ public class UserRestController {
 	public ResponseEntity<List<User>> findAllUsers() {
 		List<User> allUsers = service.getAllUsers();
 		return new ResponseEntity<>(allUsers, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/user/{id}")
+	public String deleteUserById(@PathVariable Integer id) {
+		service.deleteById(id);
+		return "delete success";
+
 	}
 
 }
